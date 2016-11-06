9ec157b6ea6b77a74b484342ab7d0a55c15c3913
beauxq <beauxq@users.noreply.github.com>
2016-11-05 20:13:53 -0500

    refactor to put user data in a service

--- Source/public/js/index.js
+++ Source/public/js/index.js
@@ -33,7 +33,54 @@ $( document ).ready(function() {
 
 angular.module("FlickBlenderApp", [])
 
-.controller("FlickBlenderController", function($scope) {
+.factory("userData", function() {
+    var data = {franchises: []};
+
+    // test data
+    data.franchises.push(new Franchise("Star Trek"));
+    data.franchises[0].serieses.push(new Series("The Next Generation"));
+    data.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 1", new Date(99, 8, 3)));
+    data.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 2", new Date(99, 8, 10)));
+    data.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 3", new Date(99, 8, 17)));
+    data.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 4", new Date(99, 8, 24)));
+    data.franchises[0].serieses.push(new Series("Deep Space Nine"));
+    data.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 1", new Date(2003, 8, 3)));
+    data.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 2", new Date(2003, 8, 10)));
+    data.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 3", new Date(2003, 8, 17)));
+    data.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 4", new Date(2003, 8, 24)));
+    data.franchises[0].serieses.push(new Series("Voyager"));
+    data.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 1", new Date(2005, 8, 3)));
+    data.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 2", new Date(2005, 8, 10)));
+    data.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 3", new Date(2005, 8, 17)));
+    data.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 4", new Date(2005, 8, 24)));
+    data.franchises.push(new Franchise("Doctor Who"));
+    data.franchises[1].serieses.push(new Series("Classic"));
+    data.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 1", new Date(99, 9, 3)));
+    data.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 2", new Date(99, 9, 10)));
+    data.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 3", new Date(99, 9, 17)));
+    data.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 4", new Date(99, 9, 24)));
+    data.franchises[1].serieses.push(new Series("New"));
+    data.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 1", new Date(2003, 9, 3)));
+    data.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 2", new Date(2003, 9, 10)));
+    data.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 3", new Date(2003, 9, 17)));
+    data.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 4", new Date(2003, 9, 24)));
+    data.franchises.push(new Franchise("Firefly"));
+    data.franchises[2].serieses.push(new Series("TV"));
+    data.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 1", new Date(2002, 9, 3)));
+    data.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 2", new Date(2002, 9, 10)));
+    data.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 3", new Date(2002, 9, 17)));
+    data.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 4", new Date(2002, 9, 24)));
+    data.franchises[2].serieses.push(new Series("Serenity"));
+    data.franchises[2].serieses[1].episodes.push(new Episode("Serenity", new Date(2005, 7, 3)));
+
+    data.addFranchise = function(newFranchiseName) {
+        data.franchises.push(new Franchise(newFranchiseName));
+    };
+
+    return data;
+})
+
+.controller("FlickBlenderController", function($scope, userData) {
     // google OAth stuff
     function onSignIn(response) {
         console.log("onSignIn function");
@@ -54,51 +101,13 @@ angular.module("FlickBlenderApp", [])
     };
 
 
-    $scope.episodes = ["Season 1 episode 1", "Season 1 episode 2", "Season 1 episode 3", "Season 1 episode 4"];
-
-    // test data
-    $scope.franchises = [];
-    $scope.franchises.push(new Franchise("Star Trek"));
-    $scope.franchises[0].serieses.push(new Series("The Next Generation"));
-    $scope.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 1", new Date(99, 8, 3)));
-    $scope.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 2", new Date(99, 8, 10)));
-    $scope.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 3", new Date(99, 8, 17)));
-    $scope.franchises[0].serieses[0].episodes.push(new Episode("TNG Season 1 episode 4", new Date(99, 8, 24)));
-    $scope.franchises[0].serieses.push(new Series("Deep Space Nine"));
-    $scope.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 1", new Date(2003, 8, 3)));
-    $scope.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 2", new Date(2003, 8, 10)));
-    $scope.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 3", new Date(2003, 8, 17)));
-    $scope.franchises[0].serieses[1].episodes.push(new Episode("DS9 Season 1 episode 4", new Date(2003, 8, 24)));
-    $scope.franchises[0].serieses.push(new Series("Voyager"));
-    $scope.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 1", new Date(2005, 8, 3)));
-    $scope.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 2", new Date(2005, 8, 10)));
-    $scope.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 3", new Date(2005, 8, 17)));
-    $scope.franchises[0].serieses[2].episodes.push(new Episode("Voyager Season 1 episode 4", new Date(2005, 8, 24)));
-    $scope.franchises.push(new Franchise("Doctor Who"));
-    $scope.franchises[1].serieses.push(new Series("Classic"));
-    $scope.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 1", new Date(99, 9, 3)));
-    $scope.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 2", new Date(99, 9, 10)));
-    $scope.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 3", new Date(99, 9, 17)));
-    $scope.franchises[1].serieses[0].episodes.push(new Episode("Who Season 1 episode 4", new Date(99, 9, 24)));
-    $scope.franchises[1].serieses.push(new Series("New"));
-    $scope.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 1", new Date(2003, 9, 3)));
-    $scope.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 2", new Date(2003, 9, 10)));
-    $scope.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 3", new Date(2003, 9, 17)));
-    $scope.franchises[1].serieses[1].episodes.push(new Episode("New Who Season 1 episode 4", new Date(2003, 9, 24)));
-    $scope.franchises.push(new Franchise("Firefly"));
-    $scope.franchises[2].serieses.push(new Series("TV"));
-    $scope.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 1", new Date(2002, 9, 3)));
-    $scope.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 2", new Date(2002, 9, 10)));
-    $scope.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 3", new Date(2002, 9, 17)));
-    $scope.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 4", new Date(2002, 9, 24)));
-    $scope.franchises[2].serieses.push(new Series("Serenity"));
-    $scope.franchises[2].serieses[1].episodes.push(new Episode("Serenity", new Date(2005, 7, 3)));
+    $scope.userData = userData;
 
     $scope.lastFranchiseClicked = -1;  // second click toggles hiding of serieses
-    $scope.thisFranchiseHidden = $scope.franchises.map(function() {return true;});  // a true for each franchise
+    $scope.thisFranchiseHidden = userData.franchises.map(function() {return true;});  // a true for each franchise
 
     $scope.franchiseListClick = function(franchiseIndexClicked) {
-        console.log($scope.franchises[franchiseIndexClicked].name + " franchise clicked");
+        console.log(userData.franchises[franchiseIndexClicked].name + " franchise clicked");
         if (franchiseIndexClicked === $scope.lastFranchiseClicked) {
             // toggle hidden
             $scope.thisFranchiseHidden[franchiseIndexClicked] = !$scope.thisFranchiseHidden[franchiseIndexClicked]
@@ -109,12 +118,12 @@ angular.module("FlickBlenderApp", [])
 
         // TODO: show the blended episode list for this franchise
         $scope.currentEpisodeList = [{name: "this will be the blended " +
-                                            $scope.franchises[franchiseIndexClicked].name +
+                                            userData.franchises[franchiseIndexClicked].name +
                                             " episode list"}];
     };
 
     $scope.seriesListClick = function(franchiseIndexClicked, seriesIndexClicked) {
-        $scope.currentEpisodeList = $scope.franchises[franchiseIndexClicked].serieses[seriesIndexClicked].episodes;
+        $scope.currentEpisodeList = userData.franchises[franchiseIndexClicked].serieses[seriesIndexClicked].episodes;
     };
 
     $scope.episodeListClick = function(clickedObject) {
@@ -123,18 +132,28 @@ angular.module("FlickBlenderApp", [])
     }
 })
 
-.controller('searchCtrl', function($scope, $http) {
+.controller('searchCtrl', function($scope, $http, userData) {
     $scope.showName = "";
+
     $scope.getShow = function() {
-
-        $http.get("https://api.themoviedb.org/3/search/multi?query=" + $scope.showName + "&api_key=2f4c29e5d9bbf6c3e34220d46d0595b0")
-
-            .then(function(response) {
-                $scope.movies = response.data.results
-            }, function(response) {
-                console.log("error response from api");
-                console.log(response);
-                $scope.movies= [];
-            });
+        $http.get("https://api.themoviedb.org/3/search/multi?query=" +
+                  $scope.showName +
+                  "&api_key=2f4c29e5d9bbf6c3e34220d46d0595b0")
+        .then(function(response) {
+            $scope.movies = response.data.results
+        }, function(response) {
+            console.log("error response from api");
+            console.log(response);
+            $scope.movies= [];
+        });
     };
+})
+
+.controller('newFranchiseCtrl', function($scope, userData) {
+    $scope.franchiseName = "";
+    $scope.addFranchiseClick = function() {
+        console.log("adding " + $scope.franchiseName);
+        userData.addFranchise($scope.franchiseName);
+        $("#newFranchiseModal").modal('hide');
+    }
 });
