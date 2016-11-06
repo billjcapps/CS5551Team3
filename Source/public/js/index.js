477f4c2072dfdd76d503c50727e2f5c4b16ed50e
beauxq <beauxq@users.noreply.github.com>
2016-11-06 17:05:44 -0600

    get episode list from server

--- Source/public/js/index.js
+++ Source/public/js/index.js
@@ -2,6 +2,10 @@
  * JavaScript for index.html
  */
 
+var MAIN_API_ADDRESS = "https://api.themoviedb.org/3";
+var API_KEY = "api_key=2f4c29e5d9bbf6c3e34220d46d0595b0";
+
+// data structures
 function Franchise(_name) {
     this.name = _name;
     this.serieses = [];
@@ -13,13 +17,21 @@ Franchise.prototype.addSeries = function(name, id) {
     }
 };
 Franchise.prototype.hasSeries = function(id) {
-    console.log("checking if " + this.name + " Franchise has id " + id);
+    // console.log("checking if " + this.name + " Franchise has id " + id);
     for (var i = 0; i < this.serieses.length; ++i) {
         if (this.serieses[i].id == id) {
             return true;
         }
     }
     return false;
+};
+Franchise.prototype.getSeriesById = function(id) {
+    for (var i = 0; i < this.serieses.length; ++i) {
+        if (this.serieses[i].id == id) {
+            return this.serieses[i];
+        }
+    }
+    return null;
 };
 
 function Series(_name, _id) {
@@ -29,6 +41,7 @@ function Series(_name, _id) {
 }
 
 function Episode(_name, _airDate) {
+    // console.log("constructing episode " + _name + " with date " + _airDate);
     this.name = _name;
     this.airDate = _airDate;
 }
@@ -66,16 +79,76 @@ angular.module("FlickBlenderApp", [])
 
 // for sharing information between controllers about which franchise we are working with
 .factory("workingFranchise", function() {
-    var data = {
+    return {
         index: -1,
         searchText: ""
     };
-    return data;
+})
+
+// all of the season API urls needed to get all the episodes
+.factory('seasonAPICalls', function() {
+    var _urls = [];
+    var _responseEpisodeList = [];  // also provides a storage place for the response
+
+    return {
+        addUrl: function(tvShowId, seasonNumber) {
+            _urls.push(MAIN_API_ADDRESS + "/tv/" + tvShowId + "/season/" + seasonNumber + "?" + API_KEY)
+        },
+        getUrls: function() { return _urls; },
+        clearUrls: function () { _urls = []; },
+
+        clearEpisodeList: function() { _responseEpisodeList = []; },
+        addToEpisodeList: function(episode) {
+            _responseEpisodeList.push(episode)
+        },
+        getEpisodeList: function() { return _responseEpisodeList; }
+    };
+})
+
+// make all the api calls to get all the episodes for a series
+// http://stackoverflow.com/questions/23560570/javascript-angular-how-to-chain-unknown-number-of-promises
+// precondition: fill seasonAPICalls
+.factory('getEpisodeListService', function($http, $q, seasonAPICalls){
+    var promises = [];
+
+    var updatePromises = function() {
+        promises = [];
+        seasonAPICalls.clearEpisodeList();
+        angular.forEach(seasonAPICalls.getUrls(), function(url){
+            promises.push(
+                $http({ method: 'GET', url: url }).then(function(seasonInfo){
+                    // console.log("got this seasonInfo from api:");
+                    // console.log(seasonInfo);
+                    angular.forEach(seasonInfo.data.episodes, function(episode) {
+                        // console.log("found this episode in seasonInfo:");
+                        // console.log(episode);
+                        var dateInfo = episode.air_date.split('-');
+                        var airDate = new Date(dateInfo[0], dateInfo[1], dateInfo[2]);
+                        // TODO: get more info about episode
+                        seasonAPICalls.addToEpisodeList(new Episode(episode.name, airDate));
+                    });
+                    // console.log("promise should now be resolved for: " + url);
+                })
+            );
+        });
+        // console.log("length of promises is now: " + promises.length);
+    };
+
+    return {
+        getEpisodes: function(callback) {
+            updatePromises();
+            $q.all(promises).then(callback());  /* function() {
+                console.log("in the factory object function");  // this never happens
+                return seasonAPICalls.getEpisodeList();
+            })*/
+        }
+    };
 })
 
 .factory("userData", function() {
     var data = {franchises: []};
 
+    /*
     // test data
     data.franchises.push(new Franchise("Star Trek"));
     data.franchises[0].serieses.push(new Series("The Next Generation"));
@@ -112,6 +185,7 @@ angular.module("FlickBlenderApp", [])
     data.franchises[2].serieses[0].episodes.push(new Episode("FF Season 1 episode 4", new Date(2002, 9, 24)));
     data.franchises[2].serieses.push(new Series("Serenity"));
     data.franchises[2].serieses[1].episodes.push(new Episode("Serenity", new Date(2005, 7, 3)));
+    */
 
     data.addFranchise = function(newFranchiseName) {
         data.franchises.push(new Franchise(newFranchiseName));
@@ -170,24 +244,24 @@ angular.module("FlickBlenderApp", [])
 
     $scope.seriesListClick = function(franchiseIndexClicked, seriesIndexClicked) {
         $scope.currentEpisodeList = userData.franchises[franchiseIndexClicked].serieses[seriesIndexClicked].episodes;
+        $scope.lastFranchiseClicked = -1;
     };
 
     $scope.episodeListClick = function(clickedObject) {
-        // TODO:
+        // TODO: episode click
         console.log(clickedObject);
     }
 })
 
-.controller('searchCtrl', function($scope, $http, userData, workingFranchise) {
+.controller('searchCtrl', function($scope, $http, userData, workingFranchise, seasonAPICalls, getEpisodeListService) {
     $scope.searchResults = [];
     $scope.workingFranchise = workingFranchise;
 
     $scope.searchButtonClick = function() {
         console.log("clicked search button working with franchise " +
                     userData.franchises[workingFranchise.index].name);
-        $http.get("https://api.themoviedb.org/3/search/multi?query=" +
-                  workingFranchise.searchText +
-                  "&api_key=2f4c29e5d9bbf6c3e34220d46d0595b0")
+        $http.get(MAIN_API_ADDRESS + "/search/multi?query=" +
+                  workingFranchise.searchText + "&" + API_KEY)
         .then(function(response) {
             $scope.searchResults = response.data.results;
             $scope.added = function(id) {
@@ -201,6 +275,8 @@ angular.module("FlickBlenderApp", [])
     };
 
     $scope.searchResultClick = function(seriesResultIndex) {
+        // TODO: show user an indication that this series is in the process of being added (it'll take a few seconds)
+
         var seriesResult = $scope.searchResults[seriesResultIndex];
         console.log(seriesResult);
         var id = seriesResult.id;
@@ -211,11 +287,36 @@ angular.module("FlickBlenderApp", [])
         console.log(name);
         userData.franchises[workingFranchise.index].addSeries(name, id);
 
-        // TODO: add episodes for this series
-        // TODO: remake blended episode list
+        // add episodes (or update episode list) for this series
 
-        // show in search results indication that this series was added
-        $scope.$digest();
+        // TODO: tell the user that this is how you update the episode list
+        // (click on the series in the search results)
+        // (or make a different way of updating it)
+
+        // get number of seasons
+        $http.get(MAIN_API_ADDRESS + "/tv/" + id + "?" + API_KEY)
+        .then(function(response) {
+            var seasonCount = response.data.number_of_seasons;
+            // prepare set of urls to call
+            seasonAPICalls.clearUrls();
+            for (var i = seasonCount; i > 0; --i) {
+                seasonAPICalls.addUrl(id, i);
+            }
+            // call those urls
+            console.log("calling getEpisodes now...");
+            getEpisodeListService.getEpisodes(function() {
+                console.log("this is what getEpisodes gave me: ");
+                console.log(seasonAPICalls.getEpisodeList());
+                // TODO: sort this list by air date
+                // put episode list in userData
+                userData.franchises[workingFranchise.index].getSeriesById(id).episodes = seasonAPICalls.getEpisodeList();
+
+                // TODO: remake blended episode list
+
+                // show in search results indication that this series was added
+                $scope.$digest();
+            });
+        });
     };
 })
 
